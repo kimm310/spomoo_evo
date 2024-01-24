d5df186f0248f3e21b694e78d8363a46de02b103
davidstk1311 <david.schittko@ruhr-uni-bochum.de>
2024-01-24 21:36:42 +0100

    added water track activity

---
+++ app/src/main/java/com/example/spomoo/waterTrack.java
@@ -0,0 +1,14 @@
+package com.example.spomoo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class waterTrack extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_water_track);
+    }
+}
